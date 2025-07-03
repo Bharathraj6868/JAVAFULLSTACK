abea131da9662b7254a8e071cb4356719da6f9da
Bharath Raj <75467184+Bharathraj6868@users.noreply.github.com>
2025-07-03 07:42:10 +0530

    Update FitnessGoalTracker.java

--- FitnessGoalTracker.java
+++ FitnessGoalTracker.java
@@ -1,5 +1,5 @@
 class FitnessGoalTracker {
-    public void main(String[] args) {
+    public static void main(String[] args) {
         updateGoalProgress(5, 10);
         updateGoalProgress(10, 8);
         updateGoalProgress(8, 12);
