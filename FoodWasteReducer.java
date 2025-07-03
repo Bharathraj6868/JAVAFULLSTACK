9545cd34d52b85116ca4a3bc2a540d00aedcdcb6
Bharath Raj <75467184+Bharathraj6868@users.noreply.github.com>
2025-07-03 07:42:31 +0530

    Update FoodWasteReducer.java

--- FoodWasteReducer.java
+++ FoodWasteReducer.java
@@ -1,5 +1,5 @@
 class FoodWasteReducer {
-    public void main(String[] args) {
+    public static void main(String[] args) {
         suggestMealPlan(10, 5);
         suggestMealPlan(15, 4);
         suggestMealPlan(12, 6);
