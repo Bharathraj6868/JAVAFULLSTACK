293b1cb8338825c3fcd1146e5be52bdadc4a1fcc
Bharath Raj <75467184+Bharathraj6868@users.noreply.github.com>
2025-07-03 07:43:32 +0530

    Update RemoteVotingSystem.java

--- RemoteVotingSystem.java
+++ RemoteVotingSystem.java
@@ -1,5 +1,5 @@
 class RemoteVotingSystem {
-    public void main(String[] args) {
+    public static void main(String[] args) {
         verifyVoterIdentity(100, 50);
         verifyVoterIdentity(200, 40);
         verifyVoterIdentity(150, 60);
